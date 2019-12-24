981144e37b4f93080689337ad1dd74cca8155dff
lzhjiuzhi <lzhjiuzhi@gmail.com>
2019-12-24 17:31:56 +0900

    log-in-ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+      # debugger
+  end
+
+  def create
+    @user = User.new(user_params) # 不是最终的实现方式
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      #           == user_url(@user)
+# 处理注册成功的情况
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email, :password, :password_confirmation)
+    end
 end
