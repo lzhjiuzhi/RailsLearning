549583593d872b5d293eef2f5ac275c46bb92da0
lzhjiuzhi <lzhjiuzhi@gmail.com>
2019-12-26 16:41:15 +0900

    Implement basic log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
     render html: "hello,rails!"
 
   end
+
+  include SessionsHelper
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    #session不是模型 不是activeRecord
+
+  end
+
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+# 登入用户，然后重定向到用户的资料页面
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 不完全正确
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
